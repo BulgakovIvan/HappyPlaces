aec963201c0d9183df4d3e113adb95f6b2faba05
BulgakovIvan <bulgivan@mail.ru>
2021-06-02 10:16:09 +0300

    Add getHappyPlaceList

--- app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.happyplaces.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.happyplaces.R
+import com.example.happyplaces.database.DatabaseHandler
+import com.example.happyplaces.models.HappyPlaceModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +17,24 @@ class MainActivity : AppCompatActivity() {
         findViewById<FloatingActionButton>(R.id.fabAddHappyPlaces).setOnClickListener {
             val intent = Intent(this, AddHappyPlaceActivity::class.java)
             startActivity(intent)
+        }
+
+        getHappyPlaceListFromLocalDB()
+    }
+
+    private fun getHappyPlaceListFromLocalDB() {
+        val dbHandler = DatabaseHandler(this)
+        // TODO: 02.06.2021 delete fun 
+//        dbHandler.deleteDataFromHappyPLaceTable()
+        val getHappyPlaceList: ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
+
+        if (getHappyPlaceList.size > 0) {
+            for (i in getHappyPlaceList) {
+                Log.e("ups", "${i.title}")
+                Log.e("ups", "${i.image}")
+            }
+        } else {
+            Log.e("ups", "empty table")
         }
     }
 }
