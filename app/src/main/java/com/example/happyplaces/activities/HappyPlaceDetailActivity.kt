53437b223a84d27070e1ecebbcd2ff47cfe47040
BulgakovIvan <bulgivan@mail.ru>
2021-06-03 12:38:07 +0300

    HappyPlaceDetailActivity get data; Serializable

--- app/src/main/java/com/example/happyplaces/activities/HappyPlaceDetailActivity.kt
+++ app/src/main/java/com/example/happyplaces/activities/HappyPlaceDetailActivity.kt
@@ -1,12 +1,38 @@
 package com.example.happyplaces.activities
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.happyplaces.R
+import com.example.happyplaces.databinding.ActivityHappyPlaceDetailBinding
+import com.example.happyplaces.models.HappyPlaceModel
 
 class HappyPlaceDetailActivity : AppCompatActivity() {
+    private lateinit var bi: ActivityHappyPlaceDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_happy_place_detail)
+        bi = ActivityHappyPlaceDetailBinding.inflate(layoutInflater)
+        setContentView(bi.root)
+
+        var happyPlaceDetailModel: HappyPlaceModel? = null
+
+        if (intent.hasExtra(MainActivity.EXTRA_PLACE_DETAILS)) {
+            happyPlaceDetailModel = intent.getSerializableExtra(
+                MainActivity.EXTRA_PLACE_DETAILS) as HappyPlaceModel
+        }
+
+        if (happyPlaceDetailModel != null) {
+            setSupportActionBar(bi.toolbarHappyPlaceDetail)
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.title = happyPlaceDetailModel.title
+
+            bi.toolbarHappyPlaceDetail.setNavigationOnClickListener {
+                onBackPressed()
+            }
+
+            bi.ivPlaceImage.setImageURI(Uri.parse(happyPlaceDetailModel.image))
+            bi.tvDescription.text = happyPlaceDetailModel.description
+            bi.tvLocation.text = happyPlaceDetailModel.location
+        }
     }
 }
--- app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
@@ -62,9 +62,13 @@ class MainActivity : AppCompatActivity() {
             HappyPlacesAdapter.OnClickListener {
             override fun onClick(position: Int, model: HappyPlaceModel) {
                 val intent = Intent(this@MainActivity, HappyPlaceDetailActivity::class.java)
+                intent.putExtra(EXTRA_PLACE_DETAILS, model)
                 startActivity(intent)
             }
         })
+    }
 
+    companion object {
+        var EXTRA_PLACE_DETAILS = "extra_place_details"
     }
 }
